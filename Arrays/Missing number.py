b7a4a5ab6e67fc3a7d1fac06e2314419b67c7d33
baabunaa <babunashvilibeka@gmail.com>
2023-02-08 13:11:09 +0100

    missing number

---
+++ Arrays/Missing number.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return len(nums) * (len(nums)+1) // 2 - sum(nums)
