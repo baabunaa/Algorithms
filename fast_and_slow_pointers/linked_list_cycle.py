039aa927496c1f7c428f50195c7ec3aa6c4b4d55
baabunaa <babunashvilibeka@gmail.com>
2023-02-10 16:23:29 +0100

    linked list cycle

---
+++ fast_and_slow_pointers/linked_list_cycle.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/linked-list-cycle/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next: return False
+        slow, fast = head, head.next
+
+        while fast and fast.next and fast != slow:
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow == fast
